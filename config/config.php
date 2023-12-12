4c41873407fb9a579eaf5dc55251283704a4f288
nacretion <ananjev.nikita2017@ya.ru>
2023-12-12 19:00:35 +0500

    Добавь конфигурацию токенизации

--- config/config.php
+++ config/config.php
@@ -4,5 +4,11 @@ return [
     'db' => [
         'host' => 'localhost',
         'port' => 3306
+    ],
+    'tokenization' => [
+        'secret' => 'secret',
+        'sessionExpDate' => 3600,
+        'cookieExpDate' => 3600,
+        'tokenExpDate' => 3600
     ]
 ];
