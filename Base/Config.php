7490d7b27187c32a70107d7b2853e37f72e8752d
nacretion <ananjev.nikita2017@ya.ru>
2023-12-13 11:33:03 +0500

    Обобщи функциональность загрузки

--- Base/Config.php
+++ Base/Config.php
@@ -3,6 +3,7 @@
 namespace app\Base;
 
 use app\Traits\Singleton;
+use Exception;
 
 class Config
 {
@@ -12,13 +13,50 @@ class Config
 
     public function __construct()
     {
-        $configData = include('../config/config.php');
-
-        if (is_array($configData)) {
-            $this->config = $configData;
-        } else {
-            throw new Exception('Error: config file not found');
+        $configFile = '../config/config.php';
+        $envFile = '../.env';
+        try {
+            $this->loadData($configFile, 'include');
+            $this->loadData($envFile, 'parse', 'env');
+        } catch (Exception $e) {
+            echo $e->getMessage();
         }
+    }
+
+    /**
+     * @param string $path Путь до файла, который нужно загрузить
+     * @param string $method Метод парсинга
+     * @param string|null $key Ключ конфига
+     * @throws Exception
+     */
+    private function loadData(
+        string $path,
+        string $method,
+        ?string $key = null
+    ): void
+    {
+        if (!file_exists($path)) {
+            throw new Exception($key . ' file not found');
+        }
+
+        $data = [];
+
+        if ($method === 'parse') {
+            $data = parse_ini_file($path, true);
+        } else {
+            $data = include($path);
+        }
+
+        if (!is_array($data) || empty($data)) {
+            throw new Exception($key . ' file is empty');
+        }
+
+        if ($key) {
+            $this->config[$key] = $data;
+            return;
+        }
+
+        $this->config = $data;
     }
 
     public function setConfig(array $config): void
@@ -43,9 +81,6 @@ class Config
     // решено парсить именно тут, чтобы не засорять config файл
     public function getEnvConfig(): ?array
     {
-        if (!isset($this->config['env']) && file_exists('../.env')) {
-            $this->config['env'] = parse_ini_file('../.env');
-        }
         return $this->config['env'] ?? null;
     }
 }
