61d96f52bbb8357b665479ac6dce6f30f78059c1
IKolyas <i.kolyasnikov@mail.ru>
2023-12-14 02:11:43 +0500

    999: add base views

---
+++ views/layouts/main.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>АКАДЕМИЯ 2023</title>
+</head>
+<body>
+<header>
+    <h1>Сан веб Академия 2023</h1>
+</header>
+
+<main class="content">
+    <?= /** @var string $content */
+    $content
+    ?>
+</main>
+
+<footer>
+</footer>
+</body>
+</html>
