2dce1f912151269a5cb3edc9c7674a8d787fe0d9
IKolyas <i.kolyasnikov@mail.ru>
2023-12-14 21:35:57 +0500

    998: correct

--- public/index.php
+++ public/index.php
@@ -2,25 +2,19 @@
 
 use App\Base\Application;
 use App\Base\Env;
-
+use App\Enums\EnvModeType;
+use App\Services\AppEnvMode;
 
 require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '../vendor/autoload.php';
+
+$envMode = env('APP_MODE', EnvModeType::PRODUCTION->value);
+AppEnvMode::setMode(EnvModeType::tryFrom($envMode));
 
 $config = require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "../config/main.php";
 
 function env($key, $default = null)
 {
     return Env::getInstance()->get($key) ?? $default;
-}
-
-$defaultMode = 'development';
-$envMode = env('APP_MODE', $defaultMode);
-
-if ($envMode === $defaultMode) {
-    ini_set('display_errors', 'On');
-    ini_set('display_startup_errors', 'On');
-
-    error_reporting(E_ALL);
 }
 
 function app(): Application
