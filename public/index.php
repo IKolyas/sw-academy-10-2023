d5d650e5945bb66ddfb55436f779e52dd55eb76b
nacretion <ananjev.nikita2017@ya.ru>
2023-12-14 19:04:25 +0500

    Возможность менять окружение обходя докер

--- public/index.php
+++ public/index.php
@@ -3,13 +3,25 @@
 use App\Base\Application;
 use App\Base\Env;
 
+
 require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '../vendor/autoload.php';
 
 $config = require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "../config/main.php";
+// if env_mode == prod call class
 
 function env($key, $default = null)
 {
     return Env::getInstance()->get($key) ?? $default;
+}
+
+$defaultMode = 'development';
+$envMode = env('APP_MODE', $defaultMode);
+
+if ($envMode === $defaultMode) {
+    ini_set('display_errors', 'On');
+    ini_set('display_startup_errors', 'On');
+
+    error_reporting(E_ALL);
 }
 
 function app(): Application
@@ -18,4 +30,3 @@ function app(): Application
 }
 
 app()->run($config);
-
