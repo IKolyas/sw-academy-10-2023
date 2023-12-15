91e752d84f78198d579dd2428fb0349e4ac30659
nacretion <ananjev.nikita2017@ya.ru>
2023-12-15 19:12:20 +0500

    Добавит валидацию полей для аутендификации

--- App/FormRequests/Validators/UserAuthValidator.php
+++ App/FormRequests/Validators/UserAuthValidator.php
@@ -4,13 +4,35 @@ namespace App\FormRequests\Validators;
 
 class UserAuthValidator extends AbstractValidator
 {
-    public static function validateLogin(string $login): bool
+    public static function validateLogin(?string $login): bool
     {
-        return preg_match('/^[a-zA-Z0-9]{3,30}$/u', $login);
+        return $login && preg_match('/^[a-zA-Z0-9]{3,30}$/u', $login);
     }
 
-    public static function validatePassword(string|null $login): bool
+    public static function validatePassword(?string $password): bool
     {
-        return preg_match('/^[a-zA-Z|^!-@~`}{_\[\]]{6,30}$/u', $login);
+        return $password && preg_match('/^[a-zA-Z|^!-@~`}{_\[\]]{6,30}$/u', $password);
+    }
+
+    public static function validateEmail(string $email): bool
+    {
+        return $email && filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    public static function validateName(string $name): bool
+    {
+        // only letters and one or zero "-" in one-two words
+        return $name && preg_match('/^([a-zA-Zа-яА-Я]+(-?){0,1}\s?){1,2}$/u', $name);
+    }
+
+    public static function validateField(string $field, ?string $value): bool
+    {
+        return match ($field) {
+            'login' => self::validateLogin($value),
+            'password', 'confirm_password' => self::validatePassword($value),
+            'email' => self::validateEmail($value),
+            'first_name', 'last_name' => self::validateName($value),
+            default => false,
+        };
     }
 }
