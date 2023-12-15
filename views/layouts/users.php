285fc15ea8db443bf00394236228e1faa2625bc6
FAMng <filippovam.ng@yandex.ru>
2023-12-15 11:46:52 +0500

    3: user table output

---
+++ views/layouts/users.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../style/users.css">
+    <title>ПОЛЬЗОВАТЕЛИ АКАДЕМИЯ 2023</title>
+</head>
+<body>
+<header>
+    <h1>ПОЛЬЗОВАТЕЛИ "АКАДЕМИЯ 2023"</h1>
+</header>
+
+<main class="content">
+
+    <?= /** @var string $content */
+    $content
+    ?>
+</main>
+
+<footer>
+</footer>
+</body>
+</html>
