ecae011ab22c22f2f997bfe390e84d9a949da581
IKolyas <i.kolyasnikov@mail.ru>
2023-12-16 16:35:37 +0500

    correct fixed

--- App/Models/User.php
+++ App/Models/User.php
@@ -7,12 +7,16 @@ use App\Repositories\UserRepository;
 class User extends AbstractModel
 {
     public int $id;
-    public string $first_name;
-    public string $last_name;
+    public ?string $first_name;
+    public ?string $last_name;
     public string $login;
     public string $password;
     public string $email;
     public int $is_admin;
+    public ?string $access_token;
+    public bool $status;
+    public string $created_at;
+    public string $updated_at;
 
     public function __construct()
     {
