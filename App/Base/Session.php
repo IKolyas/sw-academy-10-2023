0cd101f1f6cd9a6c61fe111b2ff438002a80ed24
nacretion <ananjev.nikita2017@ya.ru>
2023-12-16 18:57:29 +0500

    Добавит возможность добавления поля в объект сессии

--- App/Base/Session.php
+++ App/Base/Session.php
@@ -2,19 +2,25 @@
 
 namespace App\Base;
 
-use App\Traits\Singleton;
-
 class Session implements SessionInterface
 {
 
     public function __construct()
     {
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            return;
+        }
         session_start();
     }
 
     public function get(string $key): mixed
     {
         return $_SESSION[$key] ?? null;
+    }
+
+    public function add(string $key, mixed $value): void
+    {
+        $_SESSION[$key] = $value;
     }
 
     public function set(string $key, mixed $value): void
