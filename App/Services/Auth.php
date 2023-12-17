0d270fcd012eafbec6ecc983e250b47d36778fae
nacretion <ananjev.nikita2017@ya.ru>
2023-12-17 22:39:46 +0500

    Реализует возможность авторизации/регистрации

--- App/Services/Auth.php
+++ App/Services/Auth.php
@@ -15,10 +15,31 @@ class Auth
         $this->session = new Session();
     }
 
-    public function authorized(array $data): bool
+    public function isAuthorized(): bool
+    {
+        $token = app()->cookie->getCookie('token') ?? $this->session->get('token');
+
+        if (!$token) {
+            return false;
+        }
+
+        $users = new User();
+
+        $user = $users->find($token, 'access_token');
+
+        if (!$user) {
+            app()->cookie->removeCookie('token');
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public function tryLogin(array $data): bool
     {
         $this->authorize($data);
-        return $this->session->get('user_id');
+        return $this->isAuthorized();
     }
 
     public function authorize(array $data): void
@@ -28,20 +49,70 @@ class Auth
         $user = $users->find($data['login'], 'login');
 
         if (!$user) {
-            $this->session->add('errors', ['user' => 'Неверные данные']);
+            $this->session->addToArray('errors', ['user' => 'Неверные данные']);
+            return;
         }
 
         $passMatch = password_verify($data['password'], $user->password);
 
         if (!$passMatch) {
-            $this->session->add('errors', ['user' => 'Неверные данные']);
+            $this->session->addToArray('errors', ['user' => 'Неверные данные']);
+            return;
         }
 
         (new Session())->set('user_id', $user->id);
         $userToken = md5(uniqid());
 
-        // add cookie to params
         app()->cookie->setCookie('token', $userToken);
-
+        $this->session->set('token', $userToken);
+        $users->update(['id' => $user->id, 'access_token' => $userToken]);
     }
+
+    public function logout(): void
+    {
+        $token = app()->cookie->getCookie('token') ?? $this->session->get('token');
+
+        $users = new User();
+
+        $user = $users->find($token, 'access_token');
+        $users->update(['id' => $user->id, 'access_token' => null]);
+
+        $this->session->remove('token');
+        app()->cookie->removeCookie('token');
+    }
+
+    public function register(array $data): bool
+    {
+
+        $user = new User();
+
+        $password_dump = $data['password'];
+        $data['password'] = password_hash($password_dump, PASSWORD_DEFAULT);
+
+        $matchLogin = $user->find($data['login'], 'login');
+
+        if ($matchLogin) {
+            $this->session->addToArray('errors', ['user' => 'Пользователь с таким логином уже существует']);
+            return false;
+        }
+
+        $matchEmail = $user->find($data['email'], 'email');
+
+        if ($matchEmail) {
+            $this->session->addToArray('errors', ['user' => 'Пользователь с таким email уже существует']);
+            return false;
+        }
+
+        $isCreated = $user->create($data);
+
+        if (!$isCreated) {
+            $this->session->addToArray('errors', ['user' => 'Что-то пошло не так']);
+            return false;
+        }
+
+        $data['password'] = $password_dump;
+
+        return $this->tryLogin($data);
+    }
+
 }
