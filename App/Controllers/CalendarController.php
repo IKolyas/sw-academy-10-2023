aa8cd1d790aabce4a5719e1308d6cfac4e637547
nacretion <ananjev.nikita2017@ya.ru>
2023-12-19 13:39:44 +0500

    Выведет дни выбранного месяца (+недостающие) в календарь

--- App/Controllers/CalendarController.php
+++ App/Controllers/CalendarController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controllers;
 
+use App\Base\Request;
 use App\Models\Record;
+use App\Services\Calendar;
 use Exception;
 
 class CalendarController extends AbstractController
@@ -11,13 +13,21 @@ class CalendarController extends AbstractController
     /**
      * @throws Exception
      */
-    public function actionIndex(): void
+    public function actionIndex(array $params, ?Calendar $calendar): void
     {
         $record = new Record();
         $records = $record->findAll();
 
+        $monthsFromNow = $params['monthsFromNow'] ?? 0;
+        $dates = $calendar->getFilledDates($monthsFromNow);
+        var_dump($records);
+
         if ($records) {
-            echo $this->render('calendar/index', $records);
+            echo $this->render('calendar/index', [
+                ...$records,
+                'days' => $dates,
+                'currentMonth' => date('F', strtotime($monthsFromNow . ' month')),
+            ]);
         } else {
             //TODO: Добавить обработку ошибок
             throw new Exception('Calendars not found');
