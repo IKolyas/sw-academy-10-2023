7537b72c883a8b3ac1652a519e1911829755cb1c
nacretion <ananjev.nikita2017@ya.ru>
2023-12-19 13:34:20 +0500

    Реализует сервис Calendar.php для просчета дат

---
+++ App/Services/Calendar.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Services;
+
+class Calendar
+{
+    private array $dates = [];
+
+    public function getFilledDates(int $monthsFromNow = 0): array
+    {
+        $yearMonth = $this->getDate($monthsFromNow);
+
+        for ($day = 1; $day <= date('t', strtotime($yearMonth)); $day++) {
+            $timestamp = strtotime($yearMonth . '-' . $day);
+            $date = [
+                'value' =>date('Y-m-d', $timestamp),
+                'isCurrent' => true
+            ];
+            $this->addDate($date);
+        }
+
+        $this->addPrevDays($monthsFromNow);
+        $this->addNextDays($monthsFromNow);
+
+        return $this->dates;
+    }
+
+    private function addPrevDays(int $monthsFromNow): void
+    {
+        $yearMonth = $this->getDate($monthsFromNow - 1);
+        $lastDay = date('t', strtotime($yearMonth));
+
+        for ($day = $lastDay; $day >= 24; $day--) {
+            $timestamp = strtotime($yearMonth . '-' . $day);
+
+            if (date('D', $timestamp) === 'Sun') {
+                break;
+            }
+
+            $date = [
+                'value' =>date('Y-m-d', $timestamp),
+                'isCurrent' => false
+            ];
+
+            $this->addDate($date, 'unshift');
+        }
+    }
+
+    private function addNextDays(int $monthsFromNow): void
+    {
+        $yearMonth = $this->getDate($monthsFromNow + 1);
+
+        for ($day = 1; $day <= 6; $day++) {
+
+            $timestamp = strtotime($yearMonth . '-' . $day);
+
+            if (date('D', $timestamp) === 'Mon') {
+                break;
+            }
+
+            $date = [
+                'value' =>date('Y-m-d', $timestamp),
+                'isCurrent' => false
+            ];
+
+            $this->addDate($date);
+        }
+    }
+
+    private function getDate(int $monthsFromNow, string $format = 'Y-m'): string
+    {
+        return date($format, strtotime($monthsFromNow . ' month'));
+    }
+
+    private function addDate(array $date, string $method = 'add'): void
+    {
+        match ($method) {
+            'add' => $this->dates[] = $date,
+            'unshift' => array_unshift($this->dates, $date),
+        };
+    }
+}
