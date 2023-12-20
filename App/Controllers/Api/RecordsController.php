889c866ea65c057d9a1a3b0cf19aedea161ab324
nacretion <ananjev.nikita2017@ya.ru>
2023-12-20 18:38:37 +0500

    Реализует функциональность обновления статуса

--- App/Controllers/Api/RecordsController.php
+++ App/Controllers/Api/RecordsController.php
@@ -4,20 +4,23 @@ namespace App\Controllers\Api;
 
 use App\Base\Request;
 use App\Base\Response;
+use App\FormRequests\Validators\RecordValidator;
 use App\Models\Record;
 
 class RecordsController extends AbstractApiController
 {
     protected Record $records;
+    protected RecordValidator $recordValidator;
 
     public function __construct()
     {
         parent::__construct();
 
         $this->records = new Record();
+        $this->recordValidator = new RecordValidator();
     }
 
-    public function actionGetRecords(Request $request, Response $response): void
+    public function actionRecords(Request $request, Response $response): void
     {
         $data = $request->getBody();
 
@@ -35,7 +38,7 @@ class RecordsController extends AbstractApiController
         ]);
     }
 
-    public function actionGetRecord(Request $request, Response $response): void
+    public function actionRecord(Request $request, Response $response): void
     {
         $data = $request->getBody();
 
@@ -45,7 +48,37 @@ class RecordsController extends AbstractApiController
         $response->json([
             'status' => 200,
             'success' => true,
-            'data' => [$record]
+            'data' => $record
+        ]);
+    }
+
+    public function actionUpdateStatus(Request $request, Response $response): void
+    {
+        $data = $request->getBody();
+
+        $this->recordValidator->validateId($data['id']);
+        $this->recordValidator->validateStatus($data['status']);
+
+        $errors = app()->session->get('errors');
+
+        if ($errors) {
+            $response->json([
+                'status' => 400,
+                'success' => false,
+                'errors' => $errors
+            ]);
+            return;
+        }
+
+        $isUpdated = $this->records->update([
+            'id' => $data['id'],
+            'status' => $data['status']
+        ]);
+
+        $response->json([
+            'status' => $isUpdated ? 200 : 500,
+            'success' => !!$isUpdated,
+            'message' => $isUpdated ? 'Запись обновлена' : 'Не удалось обновить запись'
         ]);
     }
 }
