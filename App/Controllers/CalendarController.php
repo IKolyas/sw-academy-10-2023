58aaa57ce509825436903d3bf6ad2a78f6c86a69
Nastya <Nastya.k_2001@mail.ru>
2023-12-20 16:36:13 +0500

    Реализован функционал переключения по месяцу

--- App/Controllers/CalendarController.php
+++ App/Controllers/CalendarController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Base\Request;
 use App\Models\Record;
 use App\Services\Calendar;
+use DateTime;
 use Exception;
 
 class CalendarController extends AbstractController
@@ -15,13 +16,15 @@ class CalendarController extends AbstractController
      */
     public function actionIndex(array $params, ?Calendar $calendar): void
     {
-        $monthsFromNow = $params['monthsFromNow'] ?? 0;
+        $monthsFromNow = $params['monthsFromNow'] === null ? date('Y-m') : date('Y-m', strtotime($params['monthsFromNow']));
+
+        //var_dump($monthsFromNow);die;
         $dates = $calendar->getFilledDates($monthsFromNow);
 
         if ($dates) {
             echo $this->render('calendar/index', [
                 'days' => $dates,
-                'currentMonth' => date('F', strtotime($monthsFromNow . ' month')),
+                'currentMonth' => date('F', strtotime($monthsFromNow)),
             ]);
         } else {
             //TODO: Добавить обработку ошибок
--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,9 +1,28 @@
-const monthsFromNow = +window.location.search.split('monthsFromNow=')[1] || 0;
-
+const nowDate = new Date().getFullYear() + '-' + (new Date().getMonth() + 1);
+const monthsFromNow = window.location.search.split('monthsFromNow=')[1] || nowDate;
 const goNext = () => {
-    window.location.href = `/calendar?monthsFromNow=${monthsFromNow + 1}`;
+
+    var oldDate = new Date(monthsFromNow);
+
+    //прибавляем 1 месяц к текущей дате
+    oldDate.setMonth(oldDate.getMonth() + 1);
+
+    var month = oldDate.getMonth() + 1;//индексация месяца начинается с нуля
+    if (month < 9) {
+        month = '0' + month;
+    }
+    window.location.href = `/calendar?monthsFromNow=${oldDate.getFullYear() + '-' + month}`;
 }
 
 const goPrev = () => {
-    window.location.href = `/calendar?monthsFromNow=${monthsFromNow - 1}`;
+    var oldDate = new Date(monthsFromNow);
+
+    //удаляем 1 месяц от текущей дате
+    oldDate.setMonth(oldDate.getMonth() - 1);
+
+    var month = oldDate.getMonth() + 1;//индексация месяца начинается с нуля
+    if (month < 9) {
+        month = '0' + month;
+    }
+    window.location.href = `/calendar?monthsFromNow=${oldDate.getFullYear() + '-' + month}`;
 }
