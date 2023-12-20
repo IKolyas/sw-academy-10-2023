e8a06f7f3ac88399840013f704a93f0ef18dad42
nacretion <ananjev.nikita2017@ya.ru>
2023-12-20 18:02:30 +0500

    Реализует методы получения Records

--- App/Controllers/Api/RecordsController.php
+++ App/Controllers/Api/RecordsController.php
@@ -4,29 +4,48 @@ namespace App\Controllers\Api;
 
 use App\Base\Request;
 use App\Base\Response;
+use App\Models\Record;
 
 class RecordsController extends AbstractApiController
 {
-    protected bool $requireAuth = false;
+    protected Record $records;
 
-    public function actionIndex(Request $request, Response $response): void
+    public function __construct()
     {
+        parent::__construct();
+
+        $this->records = new Record();
+    }
+
+    public function actionGetRecords(Request $request, Response $response): void
+    {
+        $data = $request->getBody();
+
+        $date = $data['date'] ?? date('Y-m');
+
+        $firstDay = $date . '-01';
+        $lastDay = $date . '-' . date('t', strtotime($date));
+
+        $records = $this->records->getByRange($firstDay, $lastDay, 'date');
 
         $response->json([
             'status' => 200,
             'success' => true,
-            'message' => 'Hello world!'
+            'data' => $records
         ]);
     }
 
-    public function actionRecords(Request $request, Response $response): void
+    public function actionGetRecord(Request $request, Response $response): void
     {
-        $data = $request->getAll();
+        $data = $request->getBody();
+
+        $date = date('Y-m-d', strtotime($data['date']));
+        $record = $this->records->find($date, 'date');
 
         $response->json([
             'status' => 200,
             'success' => true,
-            'data' => $data
+            'data' => [$record]
         ]);
     }
 }
